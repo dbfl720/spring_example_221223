d7499fd9bb7aaaf9ac0dd66f7fae2ac59a81f61c
YURI HONG <dbfl720@gmail.com>
2023-03-22 21:41:19 +0900

    Insert문 다루기 - MyBatis - ex02

--- src/main/java/com/example/lesson03/bo/ReviewBO.java
+++ src/main/java/com/example/lesson03/bo/ReviewBO.java
@@ -1,7 +1,5 @@
 package com.example.lesson03.bo;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,4 +19,21 @@ public class ReviewBO {
 	public Review getReview(int id) {  // 반드시 id가 있어야됨.  Integer 쓰면 안됨. ->null일 수 있을 때만 씀. 
 		return reviewMapper.selectReview(id);
 	}
+	
+	
+	// input: Review        output: int (성공한 행의 개수) - mybatis가 해줌.
+	public int addReview(Review review) {  // 자료형, 이름 
+		return reviewMapper.insertReview(review);
+	}
+	
+	
+	// addReviewAsField(4,"콤비네이션R", "마이켈", 5.0, "역시 맛있다!!");
+//	사실 매개변수라서 이름명 컬럼명과 동일안하게 안해도됨.
+	public int addReviewAsField(
+			int storeId111, String menu111, String userName111,
+			Double point111, String review111) {
+		
+		return reviewMapper.insertReviewAsField(storeId111, menu111, userName111, point111, review111);
+	}
+	
 }
--- src/main/java/com/example/lesson03/dao/ReviewMapper.java
+++ src/main/java/com/example/lesson03/dao/ReviewMapper.java
@@ -11,4 +11,18 @@ public interface ReviewMapper {
 
 	// mybatis에서 사용되는거라서.   @Param 
 	public Review selectReview(@Param("id")int id);  //bo 함수명을 복사해서 select명만 바꾸면 됨.
+	
+	public int insertReview(Review review);   // 쿼리랑 가까워서 insert로 명칭 바꿈.파라미터가 하나일때는 @Param 생략
+	// 몇개 성공했는지 알려주는 거임.
+	
+	
+	// @Param이 있기 때문에 하나의 map이 xml로 넘어간다.
+	// ** 여기서 중요한 것은 @Param() 안에 있는 단어가 중요 이거는 xml에서 ,#{}에 사용된 이름명임!!!. 옆에는 변수명이라 아무거나 해도됨. 
+	public int insertReviewAsField(
+			@Param("storeId") int storeId222,
+			@Param("menu") String menu222,
+			@Param("userName") String userName333,
+			@Param("point") Double point333,
+			@Param("review") String review333);
+	
 }
