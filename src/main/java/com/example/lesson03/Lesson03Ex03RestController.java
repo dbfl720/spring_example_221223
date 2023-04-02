73125548c9274a5d8370425014841e68b8fb6106
YURI HONG <dbfl720@gmail.com>
2023-04-02 22:20:19 +0900

    no message

--- src/main/java/com/example/lesson03/Lesson03Ex03RestController.java
+++ src/main/java/com/example/lesson03/Lesson03Ex03RestController.java
@@ -7,6 +7,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.lesson03.bo.ReviewBO;
 
+
+//웹 주소로 요청하면, 웹사이트 View 화면이 아닌 json 같은 데이터를 응답값으로 보내준다.
+//이러한 요청을 API(Application Programming Interface) 라고 한다.
+//@Controller + @ResponseBody가 합쳐진 어노테이션이다.
+//@ResponseBody를 생략해도 메소드에서 리턴된 데이터가 response body 영역에 넣어 보내진다.
+//API 형태로 데이터를 리턴할 경우 자주 사용한다.
 @RestController
 public class Lesson03Ex03RestController {
 	
